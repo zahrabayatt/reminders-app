ee3f363049e93c6aad85270d835a5c34919ed3f6
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-08 19:31:31 +0330

    Calling the backend and add the ReminderService

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders the Reminder item", () => {
-  render(<App />);
-  const reminderElement = screen.getByText(/reminder/i);
-  expect(reminderElement).toBeInTheDocument();
-});
