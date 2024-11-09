50dc48170a1359492e2f6aa7d5db9d721b1aaa88
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-09 15:32:46 +0330

    Building an API

--- api/routers/reminders.ts
+++ api/routers/reminders.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import CreateReminderDto from "../dtos/create-reminders";
+import Reminder from "../models/reminder";
 
 const router = Router();
+const reminders: Reminder[] = [];
 
 router.get("/", (req, res) => {
-  res.send("List of reminders");
+  res.send(reminders);
 });
 
 router.post("/", (req, res) => {
   const { title } = req.body as CreateReminderDto;
-  res.json(title);
+  const reminder = new Reminder(title);
+  reminders.push(reminder);
+  res.status(201).send(reminder);
 });
 
 export default router;
