fe980a8930baa62b2893675eca334374dd1bbb21
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-09 15:24:38 +0330

    Add reminders post api and parsing request bodies

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import remindersRouter from "./routers/reminders";
 
 const app = express();
+
+app.use(express.json());
+app.use("/reminders", remindersRouter);
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
